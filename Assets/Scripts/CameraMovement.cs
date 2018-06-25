bd27b0f57f361c620597ba076b991e7165907025
Caio <>
2018-06-25 19:32:49 -0300

    Inicial-Movimentação
    Initial Commit
    Movimentação do personagem e sprite genérico

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	/// <summary>
+	/// Screen Upper Limit.
+	/// </summary>
+	public float screenUpperLimit = 1000f;
+	public float screenLowerLimit = -1000f;
+	public float screenLeftLimit = -1000f;
+	public float screenRightLimit = 1000f;
+
+	public float lerpSpeed = 10;
+
+	public Transform cameraTarget;
+//	private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+//		camera = GetComponent<Transform>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 tempPosition = Vector2.Lerp (transform.position, cameraTarget.position, lerpSpeed);
+		transform.position.Set (tempPosition.x, tempPosition.y, transform.position.z);
+	}
+
+	bool IsWithin(Vector2 target, float up,float down,float left,float right){
+		if ((target.x < up) && (target.x > down)) {
+			if ((target.y < right) && (target.y > left)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool IsWithin(float targetx, float targety, float up,float down,float left,float right){
+		if ((targetx < up) && (targetx > down)) {
+			if ((targety < right) && (targety > left)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Movimentação do avatar.
+/// </summary>
+public class PlayerMovement : MonoBehaviour {
+	/// <summary>
+	/// Velocidade do avatar (caminhando).
+	/// </summary>
+	public float playerWalkSpd;
+	/// <summary>
+	/// Aceleração do avatar (caminhando).
+	/// </summary>
+	public float playerWalkAccel;
+	/// <summary>
+	/// Aceleração do avatar (dash).
+	/// </summary>
+	public float playerDashAccel;
+	/// <summary>
+	/// Desaceleração do avatar (caminhando/dash/dano).
+	/// </summary>
+	public float playerStopAccel;
+	/// <summary>
+	/// Zona neutra de um joystick onde o input não é computado.
+	/// (Colocar no script PlayerControls)
+	/// </summary>
+	public float joystickTolerance = 0.1f;
+
+	/// <summary>
+	/// Indica o a direção ao qual o avatar está olhando.
+	/// </summary>
+	public Vector2 playerDirection;
+
+	/// <summary>
+	/// Componentes X e Y de movimento do personagem.
+	/// </summary>
+	public Vector2 playerMovement;
+
+	/// <summary>
+	/// True para ativar a animação de caminhando.
+	/// </summary>
+	private bool isWalking = false;
+	private bool isStopping = false;
+
+	private Animator playerAnimator;
+
+
+	void Start () {
+		playerAnimator = GetComponent<Animator> ();
+	}
+	
+
+	void Update ()
+	{
+		if (isWalking) {
+			isStopping = true;
+		}
+		isWalking = false;
+
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		float inputY = Input.GetAxisRaw ("Vertical");
+		float playerSpeedDelta = playerWalkSpd * Time.deltaTime;
+
+		playerDirection.Set (0, 0);
+
+		if ((Mathf.Abs (inputX)) > joystickTolerance) {
+			playerDirection.x = inputX;
+			isWalking = true;
+//			isStopping = false;
+		}
+		if (Mathf.Abs (inputY) > joystickTolerance) {
+			playerDirection.y = inputY;
+			isWalking = true;
+//			isStopping = false;
+
+		}
+		playerAnimator.SetFloat ("SpeedX", playerDirection.x);
+		playerAnimator.SetFloat ("SpeedY", playerDirection.y);
+		playerAnimator.SetBool ("isWalking", isWalking);
+
+		if (isWalking) {
+			playerMovement = playerDirection;
+			playerMovement.Normalize ();
+			transform.Translate (playerMovement.x * playerSpeedDelta, playerMovement.y * playerSpeedDelta, 0);
+			//playerDirection.Normalize();
+			//transform.Translate (playerDirection.x * playerSpeedDelta, playerDirection.y * playerSpeedDelta, 0);
+		}else{
+			playerMovement.Set (0, 0);
+		}
+	}
+}
